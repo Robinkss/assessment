530a363d577ac3efe5bd32042d12d91fe4a38723
Robin <robin.ave28@gmail.com>
2023-10-29 10:59:19 +0100

    all requirements done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import Filter from './Filter';
 import PostList from './PostList';
+import "../styles/app.css";
 
 function App() {
-  const [posts, setPosts] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState('all'); // Initial category filter
-
-  useEffect(() => {
-    // Effect to fetch data from the mock API
-    const fetchData = async () => {
-      try {
-        const response = await fetch('/api/posts');
-        const data = await response.json();
-        //console.log('data', data["posts"][0]);
-        
-        setPosts(data['posts']);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    }
-    fetchData();
-  }, []); // Run only once on component mount
 
 /*   const filteredPosts = selectedCategory === 'all'
     ? posts
@@ -28,14 +10,7 @@ function App() {
 
   return (
     <div>
-      <h1>Post List</h1>
-      <p>Showing {posts.length} posts</p>
-      <ul>
-        {posts.map(post => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-
+      <PostList></PostList>
       {/* Add pagination component here */}
     </div>
   );
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,20 +1,117 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-function PostList({ posts }) {
+import "../styles/postList.css";
+
+function PostList() {
+  const [posts, setPosts] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('all'); // Initial category filter
+  const [categories, setCategories] = useState(new Set());
+  const [displayedPosts, setDisplayedPosts] = useState([]);
+  const [visiblePostsCount, setVisiblePostsCount] = useState(5); // Nombre de posts à afficher à la fois
+
+
+  const fetchAllCategories = async () => {
+    try {
+      const response = await fetch('/api/posts');
+      const data = await response.json();
+      let categoriesSet = new Set(); //will contained all categories names
+      data["posts"].map(post => (
+        post.categories.map(category =>(
+          categoriesSet.add(category.name)
+        ))
+      ))     
+      setCategories(categoriesSet);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('/api/posts');
+      const data = await response.json();
+      //console.log('data', data["posts"][0]);
+      
+      setPosts(data['posts']);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
+  useEffect(() => {
+    // update displayed posts when selectedCategory change
+    if (selectedCategory === 'all') {
+      setDisplayedPosts(posts);
+    } else {
+      const filteredPosts = posts.filter(post => post.categories.some(category => category.name === selectedCategory));
+      setDisplayedPosts(filteredPosts);
+    }
+    setVisiblePostsCount(5);
+  }, [selectedCategory, posts]); // Run when selectedCategory changes
+
+  useEffect(() => {
+    // Effect to fetch data from the mock API 
+    fetchData();
+    fetchAllCategories();
+  }, []); // Run only once on component mount
   if (posts.length === 0) {
     return <div>No posts to display.</div>;
   }
 
+  const handleLoadMore = () => {
+    setVisiblePostsCount(visiblePostsCount + 5); // Load 5 more posts
+  }
+  
+
+  const getGategoriesList = () => {
+    return Array.from(categories).map(category => 
+      <div>{category}</div>
+    )
+  }
+
   return (
-    <ul>
-      {posts.map(post => (
-        <li key={post.id}>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-          <p>Category: {post.category}</p>
-        </li>
-      ))}
-    </ul>
+    <div className='list-container'>
+      <div className='categoryFilter'>
+          {/* {getGategoriesList()} */}
+          <label for="filter">Filter by category:</label>
+          <select name='filter' onChange={(e) => setSelectedCategory(e.target.value)}>    
+            <option value="all">All</option>
+            {categories && Array.from(categories).map(category => 
+              <option key={category} value={category}>{category}</option>
+            )}
+          </select>
+      </div>
+      <ul>
+      {selectedCategory === 'all'
+        ? displayedPosts.slice(0, visiblePostsCount).map(post => (
+            <li key={post.id}>
+              <h2>Title: {post.title}</h2>
+              <p>Author: {post.author.name}</p>
+            </li>
+          ))
+        : displayedPosts.slice(0, visiblePostsCount).map(post => {
+            let isAdded = false;  //to avoid duplicate posts
+            return post.categories.map(category => {        
+              if (category.name === selectedCategory && !isAdded) {
+                console.log('isAdded', isAdded);
+                isAdded = true; 
+                return (
+                  <li key={post.id}>
+                    <h2>Title: {post.title}</h2>
+                    <p>Author: {post.author.name}</p>
+                  </li>
+                );
+              }
+              return null;
+            });
+          })}
+        </ul>
+        {visiblePostsCount < displayedPosts.length && (
+          <button onClick={handleLoadMore}>Load more</button>
+        )}
+    </div>
+    
   );
 }
 
